3f291060f359fd506f734429115d7ce831bd45b5
Igor Fabri Cervelin <36662788+Rowzerdev@users.noreply.github.com>
2019-05-24 11:16:34 -0300

    Facial recognition project - Source code

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -3,15 +3,19 @@ import cv2, os
 BLUE_COLOR = (255, 0, 0)
 STROKE = 2
 
-xml_path = 'haarcascade_frontalface_alt2.xml'
+xml_path = 'glasses.xml'
 clf = cv2.CascadeClassifier(xml_path)
 cap = cv2.VideoCapture(0)
 
+cap.set(3, 300)
+cap.set(4, 300)
+print('W: ' +str(cap.get(3)))
+print('H: ' +str(cap.get(4)))
 while(not cv2.waitKey(20) & 0xFF == ord('q')):
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = clf.detectMultiScale(gray)
-        for x, y, w, h in faces:
+        recognize  = clf.detectMultiScale(gray)
+        for x, y, w, h in recognize:
             cv2.rectangle(frame, (x, y), (x+w, y+h), BLUE_COLOR, STROKE)
         cv2.imshow('frame',frame)
 
