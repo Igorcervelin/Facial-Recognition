947dd6c963c2a60daf69ec636ff6b3c5368f80ef
Igor Fabri Cervelin <36662788+Rowzerdev@users.noreply.github.com>
2019-05-24 13:42:25 -0300

    Facial recognition - source code

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -1,7 +1,7 @@
 import cv2, os
 
-BLUE_COLOR = (255, 0, 0)
-STROKE = 2
+SQUARE_COLOR = (255, 0, 0)
+SQUARE_SIZE = 2
 
 xml_path = 'glasses.xml'
 clf = cv2.CascadeClassifier(xml_path)
@@ -11,12 +11,13 @@ cap.set(3, 300)
 cap.set(4, 300)
 print('W: ' +str(cap.get(3)))
 print('H: ' +str(cap.get(4)))
+
 while(not cv2.waitKey(20) & 0xFF == ord('q')):
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         recognize  = clf.detectMultiScale(gray)
         for x, y, w, h in recognize:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), BLUE_COLOR, STROKE)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), SQUARE_COLOR, SQUARE_SIZE)
         cv2.imshow('frame',frame)
 
 cap.release()
